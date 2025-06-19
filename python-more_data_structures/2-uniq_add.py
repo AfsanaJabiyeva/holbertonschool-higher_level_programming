691474785abad2faaa08d09ce6b3e42e1109259e
AfsanaJabiyeva <cebiyevaefsane737@gmail.com>
2025-06-19 10:33:43 +0000

    add unique

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 
-result = 0
-
-
 def uniq_add(my_list=[]):
-    unique = set(my_list)
-    for num in unique:
-        result += num
+    return sum(set(my_list))
 
 
-return result
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 1, 4, 2, 5]
+    result = uniq_add(my_list)
+    print(f"Result: {result}")
