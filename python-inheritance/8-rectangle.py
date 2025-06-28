e146a49e8f9cb073848e48cf7d77bbf17fa331f7
Afsanajabiyeva <cebiyevaefsane737@gmail.com>
2025-06-28 15:52:04 +0000

    Rectangle

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Rectangle module."""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle with validated width and height."""
+
+    def __init__(self, width, height):
+        """Initialize with width and height as positive integers."""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
