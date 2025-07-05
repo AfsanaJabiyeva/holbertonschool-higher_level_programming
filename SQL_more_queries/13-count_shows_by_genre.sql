2cfce8c27991da77e3391f17245b09952729c210
AfsanaJabiyeva <cebiyevaefsane737@gmail.com>
2025-07-05 12:00:18 +0000

    countshows by genre

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,3 +1,6 @@
 -- Number of shows by genre
 SELECT tv_genres AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
-ORDER BY number_of_shows DESC; 
+FROM tv_genres, tv_show_genres
+WHERE tv_genre.id = tv_show_genres.genre_id
+GROUP BY tv_genre.name
+ORDER BY number_of_shows DESC;
