07e27ab76480a702f72270123751f99ff9c7c9c4
AfsanaJabiyeva <cebiyevaefsane737@gmail.com>
2025-07-05 08:10:56 +0000

    cities

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- create table with primary and foreign keys
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+
+CREATE TABLE cities(
+	id INT PRIMARY KEY AUTO_GENERATED UNIQUE NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	state_id INT,
+	FOREIGN KEY(state_id) REFERENCES states(id)
+);
