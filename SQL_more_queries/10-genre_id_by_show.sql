bed390e8072fb7e01519dfd702f4e1c1cb06a0c9
AfsanaJabiyeva <cebiyevaefsane737@gmail.com>
2025-07-05 10:01:51 +0000

    genre id by show

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,5 @@
 -- Genre ID by show
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_show_genres INNER JOIN tv_shows
-ON tv_show_genres.show_id = tv_shows.id
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows, tv_show_genres
+WHERE tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
