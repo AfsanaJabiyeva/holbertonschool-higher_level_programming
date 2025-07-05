affe03b59de2adc6c4f67a54cf8eb3323c01b81f
AfsanaJabiyeva <cebiyevaefsane737@gmail.com>
2025-07-05 08:24:37 +0000

    cities

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,15 +1,16 @@
--- create table with primary and foreign keys
+-- Create database if not exists
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 
-CREATE TABLE IF NOT EXISTS states(
-	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-	name VARCHAR(256) NOT NULL
+CREATE TABLE IF NOT EXISTS states (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS cities(
-	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-	name VARCHAR(256) NOT NULL,
-	state_id INT NOT NULL,
-	FOREIGN KEY(state_id) REFERENCES states(id)
+
+CREATE TABLE IF NOT EXISTS cities (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
 );
